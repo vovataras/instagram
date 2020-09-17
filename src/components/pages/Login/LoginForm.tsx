ccc6363c3ea7f3c4f370d723aadf8993d5c62d40
Volodya Taras <proger.sdk@gmail.com>
2020-09-17 12:35:38 +0300

    Update auth pages

--- src/components/pages/Login/LoginForm.tsx
+++ src/components/pages/Login/LoginForm.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { useFormik } from 'formik'
 import { Button, TextField } from '@material-ui/core'
 import styles from '../../modules/AuthPage/style.module.scss'
+import * as Yup from 'yup'
+
+const SigninSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required')
+})
 
 const LoginForm = () => {
   const formik = useFormik({
@@ -9,29 +15,49 @@ const LoginForm = () => {
       email: '',
       password: ''
     },
+    validationSchema: SigninSchema,
     onSubmit: (values, formikHelpers) => {
       alert(JSON.stringify(values, null, 2))
       formikHelpers.setSubmitting(false)
     }
   })
+
+  const {
+    touched,
+    errors,
+    handleChange,
+    values,
+    handleSubmit,
+    handleBlur,
+    isSubmitting
+  } = formik
+
   return (
-    <form onSubmit={formik.handleSubmit} className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <TextField
         id="email"
         label="Email"
         variant="outlined"
         required
-        onChange={formik.handleChange}
-        value={formik.values.email}
+        onBlur={handleBlur('email')}
+        error={!!(errors['email'] && touched['email'])}
+        helperText={errors.email && touched.email ? errors.email : null}
+        onChange={handleChange}
+        value={values.email}
       />
       <TextField
         id="password"
         type="password"
         label="Password"
         required
+        onBlur={handleBlur('password')}
+        error={!!(errors['password'] && touched['password'])}
+        helperText={
+          errors.password && touched.password ? errors.password : null
+        }
         variant="outlined"
-        onChange={formik.handleChange}
-        value={formik.values.password}
+        onChange={handleChange}
+        value={values.password}
       />
       <Button
         type="submit"
@@ -39,7 +65,7 @@ const LoginForm = () => {
         variant="contained"
         color="primary"
         className={styles.submit}
-        disabled={formik.isSubmitting}
+        disabled={isSubmitting}
       >
         Log in
       </Button>
--- src/components/pages/Register/RegisterForm.tsx
+++ src/components/pages/Register/RegisterForm.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { useFormik } from 'formik'
 import { Button, TextField } from '@material-ui/core'
 import styles from '../../modules/AuthPage/style.module.scss'
+import * as Yup from 'yup'
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, 'Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Required')
+})
 
 const RegisterForm = () => {
   const formik = useFormik({
@@ -10,37 +22,62 @@ const RegisterForm = () => {
       email: '',
       password: ''
     },
+    validationSchema: SignupSchema,
     onSubmit: (values, formikHelpers) => {
       alert(JSON.stringify(values, null, 2))
       formikHelpers.setSubmitting(false)
     }
   })
+
+  const {
+    touched,
+    errors,
+    handleChange,
+    values,
+    handleSubmit,
+    handleBlur,
+    isSubmitting
+  } = formik
+
   return (
-    <form onSubmit={formik.handleSubmit} className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <TextField
-        id="username"
+        name="username"
         label="Username"
         variant="outlined"
         required
-        onChange={formik.handleChange}
-        value={formik.values.username}
+        onBlur={handleBlur('username')}
+        error={!!(errors.username && touched.username)}
+        helperText={
+          errors.username && touched.username ? errors.username : null
+        }
+        onChange={handleChange}
+        value={values.username}
       />
       <TextField
-        id="email"
+        name="email"
         label="Email"
         variant="outlined"
         required
-        onChange={formik.handleChange}
-        value={formik.values.email}
+        onBlur={handleBlur('email')}
+        error={!!(errors['email'] && touched['email'])}
+        helperText={errors.email && touched.email ? errors.email : null}
+        onChange={handleChange}
+        value={values.email}
       />
       <TextField
-        id="password"
+        name="password"
         type="password"
         label="Password"
         required
+        onBlur={handleBlur('password')}
+        error={!!(errors.password && touched.password)}
+        helperText={
+          errors.password && touched.password ? errors.password : null
+        }
         variant="outlined"
-        onChange={formik.handleChange}
-        value={formik.values.password}
+        onChange={handleChange}
+        value={values.password}
       />
       <Button
         type="submit"
@@ -48,7 +85,7 @@ const RegisterForm = () => {
         variant="contained"
         color="primary"
         className={styles.submit}
-        disabled={formik.isSubmitting}
+        disabled={isSubmitting}
       >
         Sign up
       </Button>
